ca4c26b8b401b4928ff430f1826c6f57b91a9fab
inganthonycaggia <anthonycaggiabarrios@gmail.com>
2016-05-20 12:53:09 -0430

    prueba
    esta es una prueba

---
+++ prueba.py
@@ -0,0 +1,6 @@
+opc ='s'
+while opc =='s':
+	operacion = raw_input('coloque1 para saber longitud - coloque 2 para saber raiz')
+	if operacion=='1':
+	palabra = raw_input('ingrese palabra')
+	print len(palabra)
